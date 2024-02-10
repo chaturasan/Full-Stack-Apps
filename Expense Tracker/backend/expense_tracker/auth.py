87eec3d38b6facd1eceb8d29374129d9493ada41
Chaturasan Ramineni <chatur4686@gmail.com>
2024-02-10 13:07:37 +0530

    Added validations for sign up and login

--- Expense Tracker/backend/expense_tracker/auth.py
+++ Expense Tracker/backend/expense_tracker/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
+from .validations import *
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -9,30 +10,29 @@ auth = Blueprint('auth', __name__)
 @auth.route('/signup', methods=['POST'])
 def signup():
     data = request.json
-    username = data.get('username')
-    password = data.get('password')
+    
+    _, err = validate_signup_data(data)
+    if err:
+        return jsonify({'error': err}), 400
 
-    if not username or not password:
-        return jsonify({'error': 'Username and password are required'}), 400
-
-    if User.query.filter_by(username=username).first():
+    if User.query.filter_by(email=data['email']).first():
         return jsonify({'error': 'Username already exists'}), 409
 
-    hashed_password = generate_password_hash(password)
-    new_user = User(username=username, password=hashed_password)
+    hashed_password = generate_password_hash(data['password'])
+    new_user = User(email=data['email'], first_name=data['first_name'], password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
-
     return jsonify({'message': 'User created successfully'}), 201
 
 @auth.route('/login', methods=['POST'])
 def login():
     data = request.json
-    username = data.get('username')
-    password = data.get('password')
-
-    user = User.query.filter_by(username=username).first()
-    if not user or not check_password_hash(user.password, password):
+    _, err = validate_login_data(data)
+    if err:
+        return jsonify({'error': err}), 400
+    
+    user = User.query.filter_by(email=data['email']).first()
+    if not user or not check_password_hash(user.password, data['password']):
         return jsonify({'error': 'Invalid username or password'}), 401
 
     access_token = create_access_token(identity=user.id)
